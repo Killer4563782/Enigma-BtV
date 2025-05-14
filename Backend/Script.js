ff88a052fe7c893467efcb94850ff0458f022eb2
Killer4563782 <98356478+Killer4563782@users.noreply.github.com>
2025-05-14 21:48:16 +0200

    [FIX] Removed loading Screen which resulted in infinit loading screen

--- Backend/Script.js
+++ Backend/Script.js
@@ -3,11 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     window.scrollTo(0, 0);
     document.documentElement.style.scrollBehavior = "";
 
-    setTimeout(() => {
-        document.getElementById("loadingScreen").style.display = "none";
-
-        const header = document.querySelector("header");
-        header.style.opacity = "1";
-        header.style.transform = "translateY(0)";
-    }, 4000);
+    const header = document.querySelector("header");
+    header.style.opacity = "1";
+    header.style.transform = "translateY(0)";
 });
